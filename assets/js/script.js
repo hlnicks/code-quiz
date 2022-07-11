270186a5f77a25be8d7008e53c5149ac824486be
hlnicks <hlnicks@icloud.com>
2022-07-10 21:34:53 -0400

    made timer function and started on rendering questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-// will need to create an additional script for highscores
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-// quiz questions
-
-
 var questions = [
     {
         title: "question 1",
@@ -66,3 +50,49 @@ var questions = [
         answer: "answer"
     },
 ];
+
+var questionList = 0;
+var holdInterval = 0;
+var timeLeft = 60;
+var timer = document.querySelector("#startBtn");
+var timerDiv = document.querySelector("#timerDiv");
+var quizContent = document.querySelector("#quizContent");
+
+
+// renders questions/answers in quizContent
+function render(questionList) {
+    quizContent.innerHTML = "";
+    makeUl.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionList].title;
+        var userAnswer = questions[questionList].choices;
+        quizContent.textContent = userQuestion;
+    }
+    userAnswer.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        quizContent.appendChild(makeUl);
+        makeUl.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+
+// triggers time countdown once start button is clicked
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            timeLeft--;
+            timerDiv.textContent = "Time: " + timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(holdInterval);
+                timesUp();
+                timerDiv.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionList);
+});
+
+
